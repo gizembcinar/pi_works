c6d463535568809179f763ebd4f1da28349acc3a
gizembcinar <gizemb.cinar@gmail.com>
2021-06-23 22:02:50 +0300

    Update GetAccessLink.py

--- GetAccessLink.py
+++ GetAccessLink.py
@@ -1,10 +1,20 @@
 import re 
-a= ['<url>https://xcd32112.smart_meter.com</url>' ,
-    '<url>https://tXh67.dia_meter.com</url>',
-    '<url>https://yT5495.smart_meter.com</url>',
-    '<url>https://ret323_TRu.crown.com</url>',
-    '<url>https://luwr3243.celsius.com</url>' ]
+import pandas as pd
 
-for i in a:
-    o = re.findall('://([\w\.]+)<', i)
-    print(o)
+def find_url(obj):
+    access_link = []
+    for i in obj:
+        access_link.append(re.findall('://([\w\.]+)<', i))
+    return access_link
+
+
+data = {'Device_Type' : ['AXO145','TRU151', 'ZOD231', 'YRT326' ,'LWR245'],
+    'Stats_Access_Link' : ['<url>https://xcd32112.smart_meter.com</url>' ,
+                           '<url>https://tXh67.dia_meter.com</url>', 
+                           '<url>https://yT5495.smart_meter.com</url>',
+                           '<url>https://ret323_TRu.crown.com</url>',
+                           '<url>https://luwr3243.celsius.com</url>'] }
+
+df = pd.DataFrame.from_dict(data)
+
+print(find_url(df['Stats_Access_Link']))
